e7ea43a0debd9ea00a411ffb625c795159f6a3de
Sunil Shah <sunilshah2416@gmail.com>
2025-01-17 17:51:03 -0500

    Next configuration changed

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  // other configurations
+  output: "export",
+  env: {
+    NEXT_PUBLIC_X_CSRFToken: process.env.NEXT_PUBLIC_X_CSRFToken,
+    NEXT_PUBLIC_BASE_URL_PROD: process.env.NEXT_PUBLIC_BASE_URL_PROD,
+  },
 };
 
 export default nextConfig;
