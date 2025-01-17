8c7f5033d301482a9b890bff39b6c3ccc82cc2d9
Sunil Shah <sunilshah2416@gmail.com>
2025-01-17 18:32:07 -0500

    adding env while building process

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  env: {
-    NEXT_PUBLIC_X_CSRFToken: process.env.NEXT_PUBLIC_X_CSRFToken,
-    NEXT_PUBLIC_BASE_URL_PROD: process.env.NEXT_PUBLIC_BASE_URL_PROD,
-  },
+  output: 'export',
+  // other configurations
 };
 
 export default nextConfig;
