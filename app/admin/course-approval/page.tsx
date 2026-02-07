41c2bdad90c2863cbbd667473c25cc414b0c6796
Okpaheifufue Caleb <56189475+Caleb-1023@users.noreply.github.com>
2026-02-07 03:46:09 -0500

    feat: course approval revamp initial commit

--- app/admin/course-approval/page.tsx
+++ app/admin/course-approval/page.tsx
@@ -1,67 +1,68 @@
 "use client";
 
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-import { getAllCoursesHistory, getCourseByDepartment } from "@/app/api/reports";
-import CourseTable from "@/app/local-components/CoursePage/CourseTable";
-import { CourseType } from "@/app/local-components/CoursePage/CourseTable";
-import { DepartmentFilter } from "@/app/local-components/CoursePage/CourseFilter";
-import { Button } from "@/components/ui/button";
+// import { getAllCoursesHistory, getCourseByDepartment } from "@/app/api/reports";
+// import CourseTable from "@/app/local-components/CoursePage/CourseTable";
+// import { CourseType } from "@/app/local-components/CoursePage/CourseTable";
+// import { DepartmentFilter } from "@/app/local-components/CoursePage/CourseFilter";
+// import { Button } from "@/components/ui/button";
 
 const CourseApproval = () => {
-  const [courseData, setCourseData] = useState<CourseType[]>([]);
-  const [filteredCourseData, setFilteredCourseData] = useState<CourseType[]>(
-    []
-  );
-  const [department, setDepartment] = useState([]);
-  const [selectedDepartment, setSelectedDepartment] = useState<string | null>(
-    null
-  );
+  // const [courseData, setCourseData] = useState<CourseType[]>([]);
+  // const [filteredCourseData, setFilteredCourseData] = useState<CourseType[]>(
+  //   []
+  // );
+  // const [department, setDepartment] = useState([]);
+  // const [selectedDepartment, setSelectedDepartment] = useState<string | null>(
+  //   null
+  // );
 
-  useEffect(() => {
-    getAllCoursesHistory().then((data) => {
-      setCourseData(data);
-      setFilteredCourseData(data);
-    });
-    getCourseByDepartment().then((data) => {
-      setDepartment(data);
-    });
-  }, []);
+  // useEffect(() => {
+  //   getAllCoursesHistory().then((data) => {
+  //     setCourseData(data);
+  //     setFilteredCourseData(data);
+  //   });
+  //   getCourseByDepartment().then((data) => {
+  //     setDepartment(data);
+  //   });
+  // }, []);
 
-  useEffect(() => {
-    filterCoursesByDepartment();
-  }, [selectedDepartment]);
+  // useEffect(() => {
+  //   filterCoursesByDepartment();
+  // }, [selectedDepartment]);
 
-  const filterCoursesByDepartment = () => {
-    if (selectedDepartment) {
-      const filteredCourses = courseData.filter((course) =>
-        course.Course_Number.includes(selectedDepartment)
-      );
-      setFilteredCourseData(filteredCourses);
-    } else {
-      setFilteredCourseData(courseData);
-    }
-  };
+  // const filterCoursesByDepartment = () => {
+  //   if (selectedDepartment) {
+  //     const filteredCourses = courseData.filter((course) =>
+  //       course.Course_Number.includes(selectedDepartment)
+  //     );
+  //     setFilteredCourseData(filteredCourses);
+  //   } else {
+  //     setFilteredCourseData(courseData);
+  //   }
+  // };
 
-  const clearFilter = () => {
-    setSelectedDepartment(null);
-  };
+  // const clearFilter = () => {
+  //   setSelectedDepartment(null);
+  // };
 
   return (
     <main className="m-4">
       <div className="flex items-center">
         <h4 className="text-xl font-semibold my-4 mr-4">Course Approval </h4>
-        <div className="flex items-center justify-space-between gap-4 mx-4">
+        {/* <div className="flex items-center justify-space-between gap-4 mx-4">
           <DepartmentFilter
             department={department}
             selectedDepartment={selectedDepartment}
             setSelectedDepartment={setSelectedDepartment}
           />
         </div>
-        <Button onClick={clearFilter}> Clear filter </Button>
+        <Button onClick={clearFilter}> Clear filter </Button> */}
       </div>
+      <p>Coming soon</p>
 
-      <CourseTable courseData={filteredCourseData} />
+      {/* <CourseTable courseData={filteredCourseData} /> */}
       {/* <DesignTest /> */}
     </main>
   );
